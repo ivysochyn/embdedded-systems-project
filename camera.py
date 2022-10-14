49c2bff1b9f22064d52592cba8e581a0a10a5982
Illia Vysochyn <bitfun6@gmail.com>
2022-10-14 14:59:59 +0200

    [#3] camera.py: Make face recognition from camera

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@
 import cv2
 import face_recognition
 import os
+import numpy as np
 
 path = "images"
 images = []
@@ -24,6 +25,27 @@ def findEncodings(images):
 
 
 encodeListKnown = findEncodings(images)
-print(len(encodeListKnown))
-print(classNames)
-print(myList)
+capture = cv2.VideoCapture(0)
+while True:
+    success, img = capture.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+        if faceDis[matchIndex] < 0.5:
+            name = classNames[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            padding = 10
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, name, (x1, y1-padding), cv2.FONT_HERSHEY_COMPLEX,
+                        1, (255, 255, 255), 1)
+
+    cv2.imshow('Camera', img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
