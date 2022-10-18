e1df88f50ef7b8503884d3634f41faa65b074d5a
Illia Vysochyn <bitfun6@gmail.com>
2022-10-18 13:11:17 +0200

    Add frame skipping

--- camera.py
+++ camera.py
@@ -26,11 +26,14 @@ def findEncodings(images):
     return encodeList
 
 
+processFrame = False
 conn = database.create_connection("camera.db")
+rectangles = list()
 with conn:
     camera = 'Camera'
     cv2.namedWindow(camera, cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty(camera, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.setWindowProperty(camera, cv2.WND_PROP_FULLSCREEN,
+                          cv2.WINDOW_FULLSCREEN)
     database.create_table(conn, database.sql_create_attendance_table)
     encodeListKnown = findEncodings(images)
     capture = cv2.VideoCapture(0)
@@ -39,31 +42,36 @@ with conn:
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-        facesCurFrame = face_recognition.face_locations(imgS)
-        encodesCurFrame = face_recognition.face_encodings(imgS,
-                                                          facesCurFrame)
+        if processFrame:
+            rectangles = []
+            facesCurFrame = face_recognition.face_locations(imgS)
+            encodesCurFrame = face_recognition.face_encodings(imgS,
+                                                              facesCurFrame)
 
-        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-            matches = face_recognition.compare_faces(encodeListKnown,
-                                                     encodeFace)
-            faceDis = face_recognition.face_distance(encodeListKnown,
-                                                     encodeFace)
-            matchIndex = np.argmin(faceDis)
-            if faceDis[matchIndex] < 0.5:
-                date = datetime.now()
-                day = date.strftime("%d/%m/%Y")
-                time = date.strftime("%H:%M")
-                name = classNames[matchIndex].upper()
-                data = (name, day, time)
-                if not(database.exists(conn, data)):
-                    database.write_to_table(conn, (name, day, time))
-                y1, x2, y2, x1 = faceLoc
-                y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-                padding = 10
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(img, name, (x1, y1-padding),
-                            cv2.FONT_HERSHEY_COMPLEX,
-                            1, (255, 255, 255), 1)
-        cv2.imshow('Camera', img)
+            for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+                matches = face_recognition.compare_faces(encodeListKnown,
+                                                         encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown,
+                                                         encodeFace)
+                matchIndex = np.argmin(faceDis)
+                if faceDis[matchIndex] < 0.5:
+                    date = datetime.now()
+                    day = date.strftime("%d/%m/%Y")
+                    time = date.strftime("%H:%M")
+                    name = classNames[matchIndex].upper()
+                    data = (name, day, time)
+                    if not(database.exists(conn, data)):
+                        database.write_to_table(conn, (name, day, time))
+                    y1, x2, y2, x1 = faceLoc
+                    y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                    rectangles.append((name, x1, y1, x2, y2))
+        for rect in rectangles:
+            name, x1, y1, x2, y2 = rect
+            padding = 10
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, name, (x1, y1-padding), cv2.FONT_HERSHEY_COMPLEX,
+                        1, (255, 255, 255), 1)
+        processFrame = not processFrame
+        cv2.imshow(camera, img)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
